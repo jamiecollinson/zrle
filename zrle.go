8d3f510ede56e033b6e8413932b6666a1b66b9de
Jamie Collinson <jamiecollinson@gmail.com>
2017-07-06 17:24:03 +0100

    Add decode

--- zrle.go
+++ zrle.go
@@ -3,6 +3,7 @@ package zrle
 import (
 	"io"
 	"encoding/binary"
+	"compress/zlib"
 )
 
 func getLength(buf io.Reader) (uint32, error) {
@@ -12,4 +13,14 @@ func getLength(buf io.Reader) (uint32, error) {
 		return 0, err
 	}
 	return binary.BigEndian.Uint32(p), nil
+}
+
+func decode(buf io.Reader, length uint32) ([]byte, error) {
+	p := make([]byte, length)
+	r, err := zlib.NewReader(buf)
+	if err != nil {
+		return []byte{}, err
+	}
+	_, err = r.Read(p)
+	return p, err
 }
--- zrle_test.go
+++ zrle_test.go
@@ -4,9 +4,10 @@ import (
 	"bytes"
 	"encoding/binary"
 	"testing"
+	"compress/zlib"
 )
 
-func TestReadsHeader(t *testing.T) {
+func TestReadsLength(t *testing.T) {
 	bs := make([]byte, 4)
 	binary.BigEndian.PutUint32(bs, 123)
 
@@ -17,5 +18,32 @@ func TestReadsHeader(t *testing.T) {
 
 	if length != 123 {
 		t.Errorf("expected 123, got: %v", length)
+	}
+}
+
+func TestDecodesZlib(t *testing.T) {
+	bs := []byte("testtesttest")
+
+	buf := &bytes.Buffer{}
+	w := zlib.NewWriter(buf)
+	w.Write(bs)
+	
+	length := uint32(buf.Len())
+
+	decoded, err := decode(buf, length)
+
+	// no error should be raised
+	if err != nil {
+		t.Error(err)
+	}
+
+	// buf should now be empty
+	if buf.Len() != 0 {
+		t.Errorf("buffer should be empty but has length: %d", buf.Len())
+	}
+
+	// decoded should be original
+	if !bytes.Equal(decoded, bs) {
+		t.Errorf("expected %v, got %v", bs, decoded)
 	}
 }
