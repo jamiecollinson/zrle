17c8028b988c58c62fd8d572cf52154283b35e00
Jamie Collinson <jamiecollinson@gmail.com>
2017-07-09 19:07:02 +0100

    Add pixel / tile functionality

--- tile.go
+++ tile.go
@@ -7,12 +7,35 @@ const (
 	TILE_HEIGHT     = 64
 )
 
+type tileConfig struct {
+	width, height int
+}
+
 type tile struct {
-	width, height, bytesPerCPixel int
-	pixels                        []cpixel
+	x, y, width, height, bytesPerCPixel int
+	pixels                              []cpixel
+}
+
+func (t tile) toPixelGrid() [][]cpixel {
+	pixels := make([][]cpixel, t.height)
+	for i := range pixels {
+		pixels[i] = make([]cpixel, t.width)
+	}
+
+	i, j := 0, 0
+	for _, pixel := range t.pixels {
+		if j >= t.width {
+			i++
+			j = 0
+		}
+		pixels[i][j] = pixel
+		j++
+	}
+	return pixels
 }
 
 func createTiles(width int, height int) (tiles []tile) {
+	x, y := 0, 0
 	for height > 0 {
 		rowWidth := width
 
@@ -32,9 +55,29 @@ func createTiles(width int, height int) (tiles []tile) {
 			}
 			rowWidth -= tileWidth
 
-			newTile := tile{width: tileWidth, height: rowHeight}
+			newTile := tile{x: x, y: y, width: tileWidth, height: rowHeight}
 			tiles = append(tiles, newTile)
+
+			x += tileWidth
 		}
+		x = 0
+		y += rowHeight
 	}
 	return
+}
+
+func tilesToPixels(width int, height int, tiles []tile) [][]cpixel {
+	pixels := make([][]cpixel, height)
+	for i := range pixels {
+		pixels[i] = make([]cpixel, width)
+	}
+	for _, tile := range tiles {
+		tilePixels := tile.toPixelGrid()
+		for i, tileRow := range tilePixels {
+			for j, pixel := range tileRow {
+				pixels[tile.y+i][tile.x+j] = pixel
+			}
+		}
+	}
+	return pixels
 }
--- tile_test.go
+++ tile_test.go
@@ -1,6 +1,7 @@
 package zrle
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -10,8 +11,9 @@ func TestCreateSingleTile(t *testing.T) {
 		t.Errorf("expected 1 tile, got %d", len(tiles))
 	}
 	t1 := tiles[0]
-	if t1.width != 64 || t1.height != 64 {
-		t.Errorf("expected height and width 64, got %v", t1)
+	expected := tile{x: 0, y: 0, width: 64, height: 64}
+	if !reflect.DeepEqual(t1, expected) {
+		t.Errorf("expected %v, got %v", expected, t1)
 	}
 }
 
@@ -21,12 +23,14 @@ func TestCreateRowOfTiles(t *testing.T) {
 		t.Fatalf("expected 2 tiles, got %d", len(tiles))
 	}
 	t1 := tiles[0]
-	if t1.width != 64 || t1.height != 64 {
-		t.Errorf("expected height and width 64, got %v", t1)
+	expected := tile{x: 0, y: 0, width: 64, height: 64}
+	if !reflect.DeepEqual(t1, expected) {
+		t.Errorf("expected %v, got %v", expected, t1)
 	}
 	t2 := tiles[1]
-	if t2.width != 64 || t2.height != 64 {
-		t.Errorf("expected height and width 64, got %v", t1)
+	expected = tile{x: 64, y: 0, width: 64, height: 64}
+	if !reflect.DeepEqual(t2, expected) {
+		t.Errorf("expected %v, got %v", expected, t2)
 	}
 }
 
@@ -36,12 +40,14 @@ func TestCreateColumnOfTiles(t *testing.T) {
 		t.Fatalf("expected 2 tiles, got %d", len(tiles))
 	}
 	t1 := tiles[0]
-	if t1.width != 64 || t1.height != 64 {
-		t.Errorf("expected height and width 64, got %v", t1)
+	expected := tile{x: 0, y: 0, width: 64, height: 64}
+	if !reflect.DeepEqual(t1, expected) {
+		t.Errorf("expected %v, got %v", expected, t1)
 	}
 	t2 := tiles[1]
-	if t2.width != 64 || t2.height != 64 {
-		t.Errorf("expected height and width 64, got %v", t2)
+	expected = tile{x: 0, y: 64, width: 64, height: 64}
+	if !reflect.DeepEqual(t2, expected) {
+		t.Errorf("expected %v, got %v", expected, t2)
 	}
 }
 
@@ -66,19 +72,70 @@ func TestCreateIrregularGridOfTiles(t *testing.T) {
 		t.Fatalf("expected 4 tiles, got %d", len(tiles))
 	}
 	t1 := tiles[0]
-	if t1.width != 64 || t1.height != 64 {
-		t.Errorf("expected width 64 and height 64, got %v", t1)
+	expected := tile{x: 0, y: 0, width: 64, height: 64}
+	if !reflect.DeepEqual(t1, expected) {
+		t.Errorf("expected %v, got %v", expected, t1)
 	}
 	t2 := tiles[1]
-	if t2.width != 16 || t2.height != 64 {
-		t.Errorf("expected width 16 and height 64, got %v", t2)
+	expected = tile{x: 64, y: 0, width: 16, height: 64}
+	if !reflect.DeepEqual(t2, expected) {
+		t.Errorf("expected %v, got %v", expected, t2)
 	}
 	t3 := tiles[2]
-	if t3.width != 64 || t3.height != 6 {
-		t.Errorf("expected width 64 and height 6, got %v", t3)
+	expected = tile{x: 0, y: 64, width: 64, height: 6}
+	if !reflect.DeepEqual(t3, expected) {
+		t.Errorf("expected %v, got %v", expected, t3)
 	}
 	t4 := tiles[3]
-	if t4.width != 16 || t4.height != 6 {
-		t.Errorf("expected width 16 and height 6, got %v", t4)
+	expected = tile{x: 64, y: 64, width: 16, height: 6}
+	if !reflect.DeepEqual(t4, expected) {
+		t.Errorf("expected %v, got %v", expected, t4)
+	}
+}
+
+func TestTileToPixelGrid_GridCase(t *testing.T) {
+	tile := tile{
+		width:  2,
+		height: 2,
+		pixels: []cpixel{
+			cpixel{0}, cpixel{1}, cpixel{2}, cpixel{3},
+		},
+	}
+	pixels := tile.toPixelGrid()
+	expected := [][]cpixel{
+		[]cpixel{cpixel{0}, cpixel{1}},
+		[]cpixel{cpixel{2}, cpixel{3}},
+	}
+	if !reflect.DeepEqual(expected, pixels) {
+		t.Errorf("expected %v, got %v", expected, pixels)
+	}
+}
+
+func TestTileToPixelGrid_ColumnCase(t *testing.T) {
+	tile := tile{
+		width:  4,
+		height: 1,
+		pixels: []cpixel{
+			cpixel{0}, cpixel{1}, cpixel{2}, cpixel{3},
+		},
+	}
+	pixels := tile.toPixelGrid()
+	expected := [][]cpixel{
+		[]cpixel{cpixel{0}, cpixel{1}, cpixel{2}, cpixel{3}},
+	}
+	if !reflect.DeepEqual(expected, pixels) {
+		t.Errorf("expected %v, got %v", expected, pixels)
+	}
+}
+
+func TestTilesToPixels(t *testing.T) {
+	tiles := []tile{
+		tile{x: 0, y: 0, width: 1, height: 1, pixels: []cpixel{cpixel{0}}},
+		tile{x: 1, y: 0, width: 2, height: 1, pixels: []cpixel{cpixel{1}, cpixel{2}}},
+	}
+	pixels := tilesToPixels(3, 1, tiles)
+	expected := [][]cpixel{[]cpixel{cpixel{0}, cpixel{1}, cpixel{2}}}
+	if !reflect.DeepEqual(expected, pixels) {
+		t.Errorf("expected %v, got %v", expected, pixels)
 	}
 }
